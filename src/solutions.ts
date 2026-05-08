b341773adadd1a9c8e1782c5b3f61e4b10727c00
Ramisamimhaz <tasnimanisa89@gmail.com>
2026-05-08 21:52:57 +0600

    second commit

--- src/solutions.ts
+++ src/solutions.ts
@@ -3,7 +3,7 @@ function filterEvenNumbers(numbers: number[]): number[] {
     return numbers.filter(num => num % 2 === 0);
 
 }
-// console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
 
 // Problem-2
 
@@ -11,8 +11,6 @@ function reverseString(str: string): string{
 
     return str.split("").reverse().join("");
 }
-
-//  console.log(reverseString("typescript"));
 
 // Problem-3
 
@@ -31,8 +29,7 @@ function reverseString(str: string): string{
     return "Number";
    }
 
-//    console.log(checkType("Hello"));
-//    console.log(checkType(42));
+
 
 
 // Problem-4
@@ -50,7 +47,68 @@ function reverseString(str: string): string{
     age : 21
  };
  const result = getProperty(user ,"name");
-//  console.log(result); 
+ 
 
   // Problem-5 
-  
+  interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+function toggleReadStatus(book: Book) {
+  return {
+    ...book,
+    isRead: true,
+  };
+}
+
+
+const myBook: Book = {
+  title: "TypeScript Guide",
+  author: "Jane Doe",
+  publishedYear: 2024,
+};
+
+
+
+
+   // Problem-6 
+  class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, grade) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+  }
+
+  const student = new Student("Alice", 20, "A");
+
+
+ 
+  // Problem- 7
+
+  function getIntersection(firstArray: number[], secondArray: number[]) {
+  const commonNumbers: number[] = [];
+
+  for (let i = 0; i < firstArray.length; i++) {
+    const currentValue = firstArray[i];
+
+    if (secondArray.includes(currentValue)) {
+      commonNumbers.push(currentValue);
+    }
+  }
+
+  return commonNumbers;
+}
+
